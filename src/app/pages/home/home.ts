056038a79d6c2c7dbe484f7c3d5a5b788d2d36b6
karleandrosilva <contatoparakarle@gmail.com>
2026-01-29 16:32:19 -0300

    Initial commit

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../../components/navbar/navbar";
+import { Footer } from '../../components/footer/footer';
+
+@Component({
+  selector: 'app-home',
+  imports: [Navbar, Footer],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+
+}
