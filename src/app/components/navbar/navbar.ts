be9b39d2f915604ca047fc06accf9e4cbaf9d1d3
karleandrosilva <contatoparakarle@gmail.com>
2026-01-29 22:35:29 -0300

    feat: adicionar navbar ao projeto

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,22 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+
+  sidebarOpen = false;
+
+  showSidebar() {
+    this.sidebarOpen = true;
+  }
+
+  hideSidebar() {
+    this.sidebarOpen = false;
+  }
 
 }
