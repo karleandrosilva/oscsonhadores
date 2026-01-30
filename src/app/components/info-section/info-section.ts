1efbdcf992cab516fb6f07ad35ac9348dcb71739
karleandrosilva <contatoparakarle@gmail.com>
2026-01-30 19:22:19 -0300

    feat: adiciona info-section

---
+++ src/app/components/info-section/info-section.ts
@@ -0,0 +1,27 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-info-section',
+  imports: [NgOptimizedImage],
+  templateUrl: './info-section.html',
+  styleUrl: './info-section.scss',
+})
+export class InfoSection {
+
+  // começa vazio (para no html colocar o que quiser) 
+
+  @Input("info-imagem") imagem = "";
+  @Input() largura : number = 0;
+  @Input() altura : number = 0;
+  @Input() alt = "";
+
+  @Input("info-imagem2") imagem2 = "";
+  @Input() largura2 : number = 0;
+  @Input() altura2 : number = 0;
+  @Input() alt2 = "";
+
+  @Input("info-titulo") titulo = "";
+  @Input("info-texto") texto = "";
+
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
 import { Footer } from '../../components/footer/footer';
 import { NgOptimizedImage } from '@angular/common';
+import { InfoSection } from '../../components/info-section/info-section';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Footer, NgOptimizedImage],
+  imports: [Navbar, InfoSection, Footer, NgOptimizedImage],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
