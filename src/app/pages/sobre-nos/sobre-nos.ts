7f57563440f3a72d11e658a5aec103e3c972e56c
karleandrosilva <contatoparakarle@gmail.com>
2026-02-03 23:30:44 -0300

    feat: adiciona info-section na pagina sobre nos

--- src/app/pages/sobre-nos/sobre-nos.ts
+++ src/app/pages/sobre-nos/sobre-nos.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Footer } from "../../components/footer/footer";
 import { Navbar } from "../../components/navbar/navbar";
 import { Header } from '../../components/header/header';
+import { InfoSection } from '../../components/info-section/info-section';
 
 @Component({
   selector: 'app-sobre-nos',
-  imports: [Footer, Header],
+  imports: [Footer, InfoSection, Header],
   templateUrl: './sobre-nos.html',
   styleUrl: './sobre-nos.scss',
 })
