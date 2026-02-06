391126c5965956c1526376379dd49c47eea83640
karleandrosilva <contatoparakarle@gmail.com>
2026-02-05 23:56:40 -0300

    feat: adiiciona footer nas páginas

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,10 +4,11 @@ import { Footer } from '../../components/footer/footer';
 import { NgOptimizedImage } from '@angular/common';
 import { InfoSection } from '../../components/info-section/info-section';
 import { NumbersSection } from '../../components/numbers-section/numbers-section';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, InfoSection, NumbersSection, Footer, NgOptimizedImage],
+  imports: [Navbar, InfoSection, NumbersSection, Footer, NgOptimizedImage, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
