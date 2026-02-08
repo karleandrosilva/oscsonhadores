dd552b8e9a0b4cc6b70f8f68281ed01ae2abe64c
karleandrosilva <contatoparakarle@gmail.com>
2026-02-08 16:25:23 -0300

    feat: adiciona sectioin pix doacao

--- src/app/pages/doacao/doacao.ts
+++ src/app/pages/doacao/doacao.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { Header } from "../../components/header/header";
 import { Footer } from "../../components/footer/footer";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-doacao',
-  imports: [Header, Footer],
+  imports: [Header, NgOptimizedImage, Footer],
   templateUrl: './doacao.html',
   styleUrl: './doacao.scss',
 })
 export class Doacao {
+
+  chavePix = '04.872.141/0001-55';
+
+  copiarPix(): void {
+    navigator.clipboard.writeText(this.chavePix)
+      .then(() => alert('Chave Pix copiada!'))
+      .catch(() => alert('Erro ao copiar'));
+  }
 
 }
